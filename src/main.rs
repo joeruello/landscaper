ef635850ec1ceb76a06e4becb82fd2caa4602eec
Joe Ruello <joeruello@gmail.com>
2023-08-23 23:39:40 +1000

    stuff

--- src/main.rs
+++ src/main.rs
@@ -93,78 +93,10 @@ impl Context {
     }
 }
 
-async fn find_files(ctx: &Context) -> Result<HashMap<String, Vec<Code>>> {
-    Ok(ctx
-        .client
-        .search()
-        .code(&format!("org:{} {}", ctx.options.org, ctx.options.find))
-        .send()
-        .await?
-        .into_iter()
-        .into_group_map_by(|f| f.repository.name.to_owned()))
-}
-
-async fn create_catalog_entry(ctx: &Context, repo: &Repository) -> Result<ChangeSet> {
-    let owner = &ctx.options.org;
-
-    let has_argo = ctx
-        .client
-        .repos(owner, &repo.name)
-        .get_content()
-        .path(".argocd.yaml")
-        .send()
-        .await
-        .context(format!("checking {}", repo.name))
-        .is_ok();
-
-    let mut entry =
-        backstage::Component::new(&repo.name, repo.description.clone().unwrap_or_default());
-
-    entry.metadata.annotations.insert(
-        "github.com/project-slug".to_owned(),
-        repo.full_name.to_owned().unwrap_or_default(),
-    );
-
-    if has_argo {
-        entry
-            .metadata
-            .annotations
-            .insert("aargocd/app-name".to_owned(), repo.name.to_owned());
-    }
-
-    Ok(Change::CreateFile {
-        path: "catalog-info.yaml".to_owned(),
-        content: serde_yaml::to_string(&entry).unwrap(),
-    }
-    .into())
-}
-
-async fn create_missing_catalog_files(ctx: &Context) -> Result<()> {
-    let owner = &ctx.options.org;
-    let repos = ctx.client.orgs(owner).list_repos().send().await?;
-    for repo in ctx.client.all_pages(repos).await? {
-        if let Some(true) = repo.archived {
-            // println!("{} is archived", &repo.name);
-            continue;
-        }
-
-        let has_catalog = ctx
-            .client
-            .repos(owner, &repo.name)
-            .get_content()
-            .path("catalog-info.yaml")
-            .send()
-            .await
-            .context(format!("checking {}", repo.name));
-
-        if has_catalog.is_err() {
-            println!("{} does not have catalog-info.yaml", &repo.name);
-            let changeset = create_catalog_entry(ctx, &repo).await?;
-            println!("{:#?}", changeset)
-        }
-    }
-
-    Ok(())
+enum Output {
+    PullRequest { url: String },
+    Skipped(),
+    DryRun(),
 }
 
 #[tokio::main]
@@ -174,26 +106,9 @@ async fn main() -> Result<()> {
         Args::parse(),
     );
 
+    // todo subcommands
     if ctx.options.foo {
-        let mut results: Vec<Output> = Vec::new();
-
-        for file in find_files(&ctx).await?.into_iter() {
-            match find_and_replace_in_repo(&ctx, file).await {
-                Ok(n) => {
-                    println!("Done.");
-                    results.push(n);
-                }
-                Err(e) => {
-                    println!("Error: {:?}\n Skipping.", e);
-                }
-            }
-        }
-
-        for results in results {
-            if let Output::PullRequest { url } = results {
-                println!("PR: {}", url);
-            }
-        }
+        find_and_replace_in_org(&ctx).await?;
     } else {
         create_missing_catalog_files(&ctx).await?;
     }
@@ -201,10 +116,39 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-enum Output {
-    PullRequest { url: String },
-    Skipped(),
-    DryRun(),
+async fn find_and_replace_in_org(ctx: &Context) -> Result<()> {
+    let mut results: Vec<Output> = Vec::new();
+
+    for file in find_files(ctx).await?.into_iter() {
+        match find_and_replace_in_repo(ctx, file).await {
+            Ok(n) => {
+                println!("Done.");
+                results.push(n);
+            }
+            Err(e) => {
+                println!("Error: {:?}\n Skipping.", e);
+            }
+        }
+    }
+
+    for results in results {
+        if let Output::PullRequest { url } = results {
+            println!("PR: {}", url);
+        }
+    }
+
+    Ok(())
+}
+
+async fn find_files(ctx: &Context) -> Result<HashMap<String, Vec<Code>>> {
+    Ok(ctx
+        .client
+        .search()
+        .code(&format!("org:{} {}", ctx.options.org, ctx.options.find))
+        .send()
+        .await?
+        .into_iter()
+        .into_group_map_by(|f| f.repository.name.to_owned()))
 }
 
 async fn find_and_replace_in_repo(
@@ -335,7 +279,7 @@ async fn apply_changes(
             Change::CreateFile { path, content } => {
                 ctx.client
                     .repos(owner, repo_name)
-                    .create_file(&path, format!("[no ci] chore: Create {}", &path), content)
+                    .create_file(&path, format!("chore: Create {}", &path), content)
                     .branch(branch_name)
                     .send()
                     .await?;
@@ -345,7 +289,7 @@ async fn apply_changes(
                     .repos(owner, repo_name)
                     .update_file(
                         &path,
-                        format!("[no ci] chore: Update {}", &path),
+                        format!("chore: Update {}", &path),
                         content,
                         &sha,
                     )
@@ -369,4 +313,67 @@ async fn apply_changes(
             .context("PR should have a html url")?
             .to_string(),
     })
+}
+
+async fn create_catalog_entry(ctx: &Context, repo: &Repository) -> Result<ChangeSet> {
+    let owner = &ctx.options.org;
+
+    let has_argo = ctx
+        .client
+        .repos(owner, &repo.name)
+        .get_content()
+        .path(".argocd.yaml")
+        .send()
+        .await
+        .context(format!("checking {}", repo.name))
+        .is_ok();
+
+    let mut entry =
+        backstage::Component::new(&repo.name, repo.description.clone().unwrap_or_default());
+
+    entry.metadata.annotations.insert(
+        "github.com/project-slug".to_owned(),
+        repo.full_name.to_owned().unwrap_or_default(),
+    );
+
+    if has_argo {
+        entry
+            .metadata
+            .annotations
+            .insert("aargocd/app-name".to_owned(), repo.name.to_owned());
+    }
+
+    Ok(Change::CreateFile {
+        path: "catalog-info.yaml".to_owned(),
+        content: serde_yaml::to_string(&entry).unwrap(),
+    }
+    .into())
+}
+
+async fn create_missing_catalog_files(ctx: &Context) -> Result<()> {
+    let owner = &ctx.options.org;
+    let repos = ctx.client.orgs(owner).list_repos().send().await?;
+    for repo in ctx.client.all_pages(repos).await? {
+        if let Some(true) = repo.archived {
+            // println!("{} is archived", &repo.name);
+            continue;
+        }
+
+        let has_catalog = ctx
+            .client
+            .repos(owner, &repo.name)
+            .get_content()
+            .path("catalog-info.yaml")
+            .send()
+            .await
+            .context(format!("checking {}", repo.name));
+
+        if has_catalog.is_err() {
+            println!("{} does not have catalog-info.yaml", &repo.name);
+            let changeset = create_catalog_entry(ctx, &repo).await?;
+            println!("{:#?}", changeset)
+        }
+    }
+
+    Ok(())
 }
